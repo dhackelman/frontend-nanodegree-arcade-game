8d0b4772cc7bf5df259045d7dd80380f2e14c62c
dhackelman <drew.hackelman@gmail.com>
2016-02-10 11:42:45 -0600

    Add Meets Specifications project files

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://www.udacity.com/course/viewer/#!/c-nd001/l-2696458597/m-2687128535) for self-checking their submission.
+Game Instructions
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+The goal is to move your character to the river. The game starts with your character at the bottom center position. 
+
+1) Use the arrow keys to move your character from tile to tile. The goal is to move your character to the river without being squished by a ladybug. 
+
+3) If the ladybug touches your character, then it will be reset to the starting position. 
+
+Submission Information: 
+
+Project 3 submission.
+
+Resources consulted:
+
+http://www.w3schools.com/js/default.asp
+https://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml#Comments
+http://stackoverflow.com/questions/395618/is-there-any-significant-difference-between-using-if-else-and-switch-case-in-c
+https://discussions.udacity.com/t/a-study-in-javascript-provided-code-for-the-game-clone/38871
+
+Author: Drew Hackelman
+
+First Submission Date: 01/30/2016
+
+
--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(initialX, initialY, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = initialX;
+    this.y = initialY;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,21 +16,122 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    //Add Collision Detection
+    this.x = this.x + this.speed * dt;
+    if (this.x > 500) {
+        // Initial enemy x-axis position
+        this.x = -60;
+        }
+    var bugXLeftRange = this.x - 50;
+    var bugXRightRange = this.x + 50;
+    var bugYTopRange = this.y - 50;
+    var bugYBottomRange = this.y + 50;
 
+    if (player.x > bugXLeftRange && player.x < bugXRightRange && player.y > bugYTopRange && player.y < bugYBottomRange) {
+        player.playerLoseReset();
+    }
+};
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+//Give Player a starting location.
+var playerInitialX = 200;
+var playerInitialY = 440;
 
+
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.x = playerInitialX;
+    this.y = playerInitialY;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+};
+
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    //Update now includes walls/limits
+    if (this.x<30) {
+        this.x =30;
+    }
+    else if (this.x>400){
+        this.x =400;
+    }
+    else if (this.y < 20 ){
+        this.y =20;
+    }
+    else if (this.y >410){
+        this.y =410;
+    }
+    // Player reset to starting position
+    else if( this.y < 30 && this.x <= 505){
+        this.playerWinReset();
+    }
+};
+
+Player.prototype.playerWinReset = function() {
+    this.x = playerInitialX;
+    this.y = playerInitialY;
+    console.log("You Win!");
+};
+
+Player.prototype.playerLoseReset = function() {
+    this.x = playerInitialX;
+    this.y = playerInitialY;
+    console.log("bug collision");
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//Create handleInput() method.
+
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+        case 'left':
+        this.x = this.x-100;
+        console.log("L");
+        break;
+        case 'right':
+        this.x = this.x+100;
+        console.log("R");
+        break;
+        case 'up':
+        this.y = this.y-90;
+        console.log("U");
+        break;
+        case 'down':
+        this.y =this.y+90;
+        console.log("D");
+        break;
+    }
+  
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+allEnemies = [];
+for (var i = 0; i < 3; i++) {
+    var tempSpeed = Math.floor(Math.random() * 5 + 1) * 55;
+    allEnemies.push(new Enemy(-60, 60 + 85 * i, tempSpeed));
+}
+
+var player = new Player();
 
 
 
